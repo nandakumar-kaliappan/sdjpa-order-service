5deb4043e3792b3ba0da47b8b8f5806a9592147e
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-09-01 10:36:13 +0530

    initial commit

---
+++ src/main/java/com/knkweb/sdjpaorderservice/SdjpaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.knkweb.sdjpaorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knkweb/sdjpaorderservice/SdjpaOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knkweb.sdjpaorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
